dfccf18e6a9ac059d44c819e282349057f66c849
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-26 01:24:44 -0800

    Temporary addon removed

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/zeros_fruitslicer_2.1.0/lua/entities/zfs_anim/shared.lua
+++
@@ -1,7 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_anim"
-ENT.AutomaticFrameAdvance = true
-ENT.Spawnable = false
-ENT.RenderGroup = RENDERGROUP_OPAQUE
-ENT.Model = "models/zerochain/fruitslicerjob/fs_melon.mdl"
-ENT.DisableDuplicator = false
