457c9856d7b2aba91cf6ea6dfc19f2cc2bcb49f2
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-27 02:10:08 +1100

    Add Ammo into F4

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
@@ -1,36 +1,36 @@
 DarkRP.createAmmoType("357", {
 	name = "357 ammo",
 	model = "models/Items/357ammobox.mdl",
-	price = 60,
+	price = 300,
 	amountGiven = 20
 })
 DarkRP.createAmmoType("ar2", {
 	name = "Rifle ammo",
 	model = "models/Items/BoxMRounds.mdl",
-	price = 60,
+	price = 1200,
 	amountGiven = 30
 })
 DarkRP.createAmmoType("buckshot", {
 	name = "Shotgun ammo",
 	model = "models/Items/BoxBuckshot.mdl",
-	price = 40,
+	price = 400,
 	amountGiven = 8
 })
 DarkRP.createAmmoType("pistol", {
 	name = "Pistol ammo",
 	model = "models/Items/BoxSRounds.mdl",
-	price = 20,
+	price = 200,
 	amountGiven = 20
 })
 DarkRP.createAmmoType("smg1", {
 	name = "SMG ammo",
 	model = "models/Items/BoxMRounds.mdl",
-	price = 40,
+	price = 400,
 	amountGiven = 60
 })
 DarkRP.createAmmoType("SniperPenetratedRound", {
 	name = "Sniper ammo",
 	model = "models/Items/BoxMRounds.mdl",
-	price = 80,
+	price = 1800,
 	amountGiven = 6
 })
---
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/ulx-master/lua/ulx/modules/sh/usercmd.lua
@@ -0,0 +1,71 @@
+-------------------------------------------------------
+-- Made By: XxLMM13xXgaming
+-------------------------------------------------------
+
+addonurl = "https://steamcommunity.com/sharedfiles/filedetails/?id=2332062788"
+discordurl = "https://discord.gg/hWN7zXtbQP"
+rulesurl = "https://bit.ly/3mQVodO"
+reporturl = "https://bit.ly/37PEQyH"
+bugsurl = "https://bit.ly/2WKFtDB"
+applyurl = "https://bit.ly/2WKFtDB"
+
+addoncommand = "!addons"
+workshopcommand = "!workshop"
+discordcommand = "!discord"
+rulescommand = "!rules"
+reportcommand = "!report"
+bugscommand = "!bugs"
+applycommand = "!apply"
+
+
+local CATEGORY_NAME = "Chat"
+
+function ulx.addons( calling_ply )
+	calling_ply:SendLua([[gui.OpenURL(addonurl)]])
+end
+local addons = ulx.command( CATEGORY_NAME, "ulx addons", ulx.addons, addoncommand )
+addons:defaultAccess( ULib.ACCESS_ALL )
+addons:help( "Opens the collection for the server." )
+
+function ulx.workshop( calling_ply )
+	calling_ply:SendLua([[gui.OpenURL(addonurl)]])
+end
+local workshop = ulx.command( CATEGORY_NAME, "ulx workshop", ulx.workshop, workshopcommand )
+workshop:defaultAccess( ULib.ACCESS_ALL )
+workshop:help( "Opens the workshop for the server." )
+
+function ulx.discord( calling_ply )
+	calling_ply:SendLua([[gui.OpenURL(discordurl)]])
+end
+local discord = ulx.command( CATEGORY_NAME, "ulx discord", ulx.discord, discordcommand )
+discord:defaultAccess( ULib.ACCESS_ALL )
+discord:help( "Opens the discord for the server." )
+
+function ulx.rules( calling_ply )
+	calling_ply:SendLua([[gui.OpenURL(rulesurl)]])
+end
+local rules = ulx.command( CATEGORY_NAME, "ulx rules", ulx.rules, rulescommand ) 
+rules:defaultAccess( ULib.ACCESS_ALL )
+rules:help( "Opens the rules for the server." )
+
+function ulx.report( calling_ply )
+	calling_ply:SendLua([[gui.OpenURL(reporturl)]])
+end
+local report = ulx.command( CATEGORY_NAME, "ulx report", ulx.report, reportcommand )
+report:defaultAccess( ULib.ACCESS_ALL )
+report:help( "Create a player report!" )
+
+function ulx.bugs( calling_ply )
+	calling_ply:SendLua([[gui.OpenURL(bugsurl)]])
+end
+local bugs = ulx.command( CATEGORY_NAME, "ulx bugs", ulx.bugs, bugscommand )
+bugs:defaultAccess( ULib.ACCESS_ALL )
+bugs:help( "Report a bug!" )
+
+function ulx.apply( calling_ply )
+	calling_ply:SendLua([[gui.OpenURL(applyurl)]])
+end
+local apply = ulx.command( CATEGORY_NAME, "ulx apply", ulx.apply, applycommand )
+apply:defaultAccess( ULib.ACCESS_ALL )
+apply:help( "Apply for staff!" )
+
