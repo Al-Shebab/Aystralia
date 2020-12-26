df7897f2d0e209065f475e14ab0fc7fff6d5e5a5
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-27 02:29:27 +1100

    Add a all ammo crate

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
@@ -1,42 +1,49 @@
 DarkRP.createAmmoType("357", {
-	name = "357 ammo",
+	name = "357 Ammo",
 	model = "models/Items/357ammobox.mdl",
 	price = 300,
-	amountGiven = 20
-    category = "Ammo"
-}),
+	amountGiven = 20,
+    category = "Ammo",
+})
 DarkRP.createAmmoType("ar2", {
-	name = "Rifle ammo",
+	name = "Rifle Ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 1200,
-	amountGiven = 30
-    category = "Ammo"
-}),
+	amountGiven = 30,
+    category = "Ammo",
+})
 DarkRP.createAmmoType("buckshot", {
-	name = "Shotgun ammo",
+	name = "Shotgun Ammo",
 	model = "models/Items/BoxBuckshot.mdl",
 	price = 400,
-	amountGiven = 8
-    category = "Ammo"
-}),
+	amountGiven = 8,
+    category = "Ammo",
+})
 DarkRP.createAmmoType("pistol", {
-	name = "Pistol ammo",
+	name = "Pistol Ammo",
 	model = "models/Items/BoxSRounds.mdl",
 	price = 200,
-	amountGiven = 20
-    category = "Ammo"
-}),
+	amountGiven = 20,
+    category = "Ammo",
+})
 DarkRP.createAmmoType("smg1", {
-	name = "SMG ammo",
+	name = "SMG Ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 400,
-	amountGiven = 60
-    category = "Ammo"
-}),
-DarkRP.createAmmoType("SniperPenetratedRound", {
-	name = "Sniper ammo",
+	amountGiven = 60,
+    category = "Ammo",
+})
+DarkRP.createAmmoType("SniperPenetratedRound", "smg1", "pistol", "buckshot", "ar2", "357", {
+	name = "Sniper Ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 1800,
-	amountGiven = 6
-    category = "Ammo"
+	amountGiven = 6,
+    category = "Ammo",
+})
+DarkRP.createAmmoType("SniperPenetratedRound", {
+	name = "All Ammo",
+	model = "models/Items/BoxMRounds.mdl",
+	price = 5000,
+	amountGiven = 30,
+    category = "Ammo",
 })
