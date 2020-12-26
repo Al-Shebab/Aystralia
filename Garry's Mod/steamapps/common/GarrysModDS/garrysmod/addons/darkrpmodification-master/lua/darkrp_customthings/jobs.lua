21f3e0857c155248f6904e36d15ae01b9b98d157
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-27 01:21:34 +1100

    Dealer jobs

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
@@ -142,7 +142,7 @@ TEAM_GAMBLING_ADDICT = DarkRP.createJob("Gambling Addict", {
 -- Services --
 
 TEAM_SECURITY_GUARD = DarkRP.createJob("Security Guard", {
-    color = Color(34, 85, 85, 255),
+    color = Color(80, 221, 204),
     model = {"models/player/odessa.mdl"},
     description = [[Hired security for any business or person. Can not raid/mug.]],
     weapons = {"weapon_cigarette_camel"},
@@ -156,7 +156,7 @@ TEAM_SECURITY_GUARD = DarkRP.createJob("Security Guard", {
     category = "Services"
 })
 TEAM_DOCTER = DarkRP.createJob("Docter", {
-    color = Color(34, 85, 85, 255),
+    color = Color(80, 221, 204),
     model = {"models/player/Group03m/male_01.mdl"},
     description = [[Paid good money to help out others in need. Can not raid/mug. Can not use weapons.]],
     weapons = {"weapon_medkit"},
@@ -170,7 +170,7 @@ TEAM_DOCTER = DarkRP.createJob("Docter", {
     category = "Services"
 })
 TEAM_NURSE = DarkRP.createJob("Nurse", {
-    color = Color(34, 85, 85, 255),
+    color = Color(80, 221, 204),
     model = {
         "models/player/Group03m/female_02.mdl",
         "models/player/Group03m/female_01.mdl",
@@ -189,3 +189,58 @@ TEAM_NURSE = DarkRP.createJob("Nurse", {
 })
 
 -- Dealers --
+
+TEAM_GUN_DEALER = DarkRP.createJob("Gun Dealer", {
+    color = Color(37, 110, 201),
+    model = {"models/player/monk.mdl"},
+    description = [[Deliver small arms to the city. Can not raid/mug.]],
+    weapons = {},
+    command = "TEAM_GUN_DEALER",
+    max = 2,
+    salary = 750,
+    admin = 0,
+    vote = true,
+    hasLicense = true,
+    candemote = true,
+    category = "Dealers"
+})
+TEAM_BLACK_MARKET_DEALER = DarkRP.createJob("Black Market Dealer", {
+    color = Color(37, 110, 201),
+    model = {"models/GrandTheftAuto5/Trevor.mdl"},
+    description = [[Deliver large arms and explosives to the city. Can not raid/mug.]],
+    weapons = {"m9k_ak47"},
+    command = "TEAM_BLACK_MARKET_DEALER",
+    max = 2,
+    salary = 1500,
+    admin = 0,
+    vote = true,
+    hasLicense = true,
+    candemote = true,
+    category = "Dealers",
+    ammo = {
+        ["AR2"] = 60
+    },
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(100)
+        ply:SetHealth(100)
+        ply:SetArmor(100)
+    end,
+    customCheck = function(ply) return
+        table.HasValue({"sydney", "melbourne", "brisbane", "perth", "adelaide", "hobart", "darwin", "superadmin", "senior-admin", "donator-admin", "donator-senior-moderator", "donator-moderator", "donator-trial-moderator"}, ply:GetNWString("usergroup"))
+    end,
+    CustomCheckFailMsg = "This job is for donators only.",
+})
+TEAM_DRUG_DEALER = DarkRP.createJob("Drug Dealer", {
+    color = Color(37, 110, 201),
+    model = {"models/player/MKX_Jax.mdl"},
+    description = [[Sell prescription drugs to those in need. Can not raid/mug.]],
+    weapons = {},
+    command = "TEAM_DRUG_DEALER",
+    max = 2,
+    salary = 1750,
+    admin = 0,
+    vote = true,
+    hasLicense = true,
+    candemote = true,
+    category = "Dealers"
+})
