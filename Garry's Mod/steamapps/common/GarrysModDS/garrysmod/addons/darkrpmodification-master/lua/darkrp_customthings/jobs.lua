1d01637326cbd8f1666fd316451e59896f058f2f
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-25 22:09:43 -0800

    Garry's Mod - Disable FAdmin, fix default jobs

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
@@ -3,16 +3,12 @@ DarkRP custom jobs
 ---------------------------------------------------------------------------
 This file contains your custom jobs.
 This file should also contain jobs from DarkRP that you edited.
-
 Note: If you want to edit a default DarkRP job, first disable it in darkrp_config/disabled_defaults.lua
       Once you've done that, copy and paste the job to this file and edit it.
-
 The default jobs can be found here:
 https://github.com/FPtje/DarkRP/blob/master/gamemode/config/jobrelated.lua
-
 For examples and explanation please visit this wiki page:
 https://darkrp.miraheze.org/wiki/DarkRP:CustomJobFields
-
 Add your custom jobs under the following line:
 ---------------------------------------------------------------------------]]
 
@@ -21,7 +17,7 @@ Add your custom jobs under the following line:
 --[[---------------------------------------------------------------------------
 Define which team joining players spawn into and what team you change to if demoted
 ---------------------------------------------------------------------------]]
-GAMEMODE.DefaultTeam = JOB_CIVILIAN
+GAMEMODE.DefaultTeam = TEAM_CITIZEN
 --[[---------------------------------------------------------------------------
 Define which teams belong to civil protection
 Civil protection can set warrants, make people wanted and do some other police related things
@@ -35,30 +31,3 @@ GAMEMODE.CivilProtection = {
 Jobs that are hitmen (enables the hitman menu)
 ---------------------------------------------------------------------------]]
 DarkRP.addHitmanTeam(TEAM_MOB)
-
---[[
-    Generated using: DarkRP | Job Generator
-    https://csite.io/tools/gmod-darkrp-job
---]]
-JOB_CIVILIAN = DarkRP.createJob("Civilian", {
-    color = Color(140, 140, 140, 255),
-    model = {
-        "models/player/Group01/female_01.mdl",
-        "models/player/Group01/female_02.mdl",
-        "models/player/Group01/female_03.mdl",
-        "models/player/Group01/female_04.mdl",
-        "models/player/Group01/male_01.mdl",
-        "models/player/Group01/male_02.mdl",
-        "models/player/Group01/male_03.mdl",
-        "models/player/Group01/male_04.mdl"
-    },
-    description = [[The Citizen is the most basic level of society you can hold besides being a hobo. You have no specific role in city life.]],
-    weapons = {},
-    command = "JOB_CIVILIAN",
-    max = 0,
-    salary = 500,
-    admin = 0,
-    vote = false,
-    hasLicense = false,
-    candemote = false
-})
