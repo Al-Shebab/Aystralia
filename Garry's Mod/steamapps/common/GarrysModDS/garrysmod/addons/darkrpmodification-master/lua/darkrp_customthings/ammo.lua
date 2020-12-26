78fef9644f39397cd50595bea10100013b23d0ee
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-27 02:22:02 +1100

    Ammo & Catagories

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
@@ -3,34 +3,40 @@ DarkRP.createAmmoType("357", {
 	model = "models/Items/357ammobox.mdl",
 	price = 300,
 	amountGiven = 20
+    category = "Ammo"
 })
 DarkRP.createAmmoType("ar2", {
 	name = "Rifle ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 1200,
 	amountGiven = 30
+    category = "Ammo"
 })
 DarkRP.createAmmoType("buckshot", {
 	name = "Shotgun ammo",
 	model = "models/Items/BoxBuckshot.mdl",
 	price = 400,
 	amountGiven = 8
+    category = "Ammo"
 })
 DarkRP.createAmmoType("pistol", {
 	name = "Pistol ammo",
 	model = "models/Items/BoxSRounds.mdl",
 	price = 200,
 	amountGiven = 20
+    category = "Ammo"
 })
 DarkRP.createAmmoType("smg1", {
 	name = "SMG ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 400,
 	amountGiven = 60
+    category = "Ammo"
 })
 DarkRP.createAmmoType("SniperPenetratedRound", {
 	name = "Sniper ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 1800,
 	amountGiven = 6
+    category = "Ammo"
 })
--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/categories.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/categories.lua
@@ -24,7 +24,7 @@ Add new categories under the next line!
 DarkRP.createCategory{
     name = "Citizens", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
     sortOrder = 1, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
@@ -32,7 +32,7 @@ DarkRP.createCategory{
 DarkRP.createCategory{
     name = "Services", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
     sortOrder = 2, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
@@ -40,7 +40,7 @@ DarkRP.createCategory{
 DarkRP.createCategory{
     name = "Dealers", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
     sortOrder = 3, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
@@ -48,7 +48,7 @@ DarkRP.createCategory{
 DarkRP.createCategory{
     name = "Government", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
     sortOrder = 4, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
@@ -56,7 +56,7 @@ DarkRP.createCategory{
 DarkRP.createCategory{
     name = "Illegal", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
     sortOrder = 5, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
@@ -64,7 +64,7 @@ DarkRP.createCategory{
 DarkRP.createCategory{
     name = "Homeless", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
     sortOrder = 6, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
@@ -72,7 +72,7 @@ DarkRP.createCategory{
 DarkRP.createCategory{
     name = "Manufacturing", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
     sortOrder = 7, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.fault.
@@ -80,9 +80,9 @@ DarkRP.createCategory{
 DarkRP.createCategory{
     name = "Donator Classes", -- The name of the category.
     categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
-    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    startExpanded = false, -- Whether the category is expanded when you open the F4 menu.
     color = Color(0, 107, 0, 255), -- The color of the category header.
-    canSee = function(ply) return table.HasValue({"sydney", "melbourne", "brisbane", "perth", "adelaide", "hobart", "darwin"}, ply:GetNWString("usergroup")) end,
+    canSee = function(ply) return table.HasValue({"sydney", "melbourne", "brisbane", "perth", "adelaide", "hobart", "darwin", "superadmin", "senior-admin", "donator-admin", "donator-senior-moderator", "donator-moderator", "donator-trial-moderator"}, ply:GetNWString("usergroup")) end,
     sortOrder = 8, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
 }
 DarkRP.createCategory{
@@ -92,4 +92,66 @@ DarkRP.createCategory{
     color = Color(0, 107, 0, 255), -- The color of the category header.
     canSee = function(ply) return table.HasValue({"superadmin", "staff-manager", "senior-admin", "admin", "senior-moderator", "moderator", "trial-moderator"}, ply:GetNWString("usergroup")) end,
     sortOrder = 9, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
+}
+
+-- Gun Catagories --
+
+DarkRP.createCategory{
+    name = "Ammo",
+    categorises = "ammo",
+    startExpanded = true,
+    color = Color(255, 0, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 1
+}
+
+
+
+DarkRP.createCategory{
+    name = "Pistols",
+    categorises = "shipments",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return table.HasValue({TEAM_GUN_DEALER, TEAM_BLACK_MARKET_DEALER}, ply:Team()) end,
+    sortOrder = 1
+}
+DarkRP.createCategory{
+    name = "SMGs",
+    categorises = "shipments",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return table.HasValue({TEAM_GUN_DEALER, TEAM_BLACK_MARKET_DEALER}, ply:Team()) end,
+    sortOrder = 2
+}
+DarkRP.createCategory{
+    name = "Rifles",
+    categorises = "shipments",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return table.HasValue({TEAM_GUN_DEALER, TEAM_BLACK_MARKET_DEALER}, ply:Team()) end,
+    sortOrder = 3
+}
+DarkRP.createCategory{
+    name = "Machine Guns",
+    categorises = "shipments",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return table.HasValue({TEAM_BLACK_MARKET_DEALER}, ply:Team()) end,
+    sortOrder = 4
+}
+DarkRP.createCategory{
+    name = "Snipers",
+    categorises = "shipments",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return table.HasValue({TEAM_BLACK_MARKET_DEALER}, ply:Team()) end,
+    sortOrder = 5
+}
+DarkRP.createCategory{
+    name = "Explosives",
+    categorises = "shipments",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return table.HasValue({TEAM_BLACK_MARKET_DEALER}, ply:Team()) end,
+    sortOrder = 6
 }
