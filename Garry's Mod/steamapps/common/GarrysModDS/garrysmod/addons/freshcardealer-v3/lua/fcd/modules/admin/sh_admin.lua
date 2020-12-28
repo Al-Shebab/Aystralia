607fa4fb95ff005094749ea1436421f5550d70e8
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-28 17:29:04 +1100

    get rid of shit car dealer

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/freshcardealer-v3/lua/fcd/modules/admin/sh_admin.lua
+++
@@ -1,5 +0,0 @@
-function fcd.adminAccess( ply )
-	if not ply then return end
-
-	return table.HasValue( fcd.cfg.adminRanks, ply:GetUserGroup() )
-end
--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/freshcardealer-v3/lua/fcd/modules/base/cl_fonts.lua
+++
@@ -1,7 +0,0 @@
-for i = 1, 150 do
-	surface.CreateFont("fcd_font_"..i, {
-		font = fcd.cfg.font,
-		size = i,
-		weight = 500
-	})
-end
