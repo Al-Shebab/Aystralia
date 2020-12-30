96e31dd845b396be2dc43b5e7c7a4fbb90b1abe1
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2020-12-30 13:07:40 +1100

    Update cm_config.lua

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/crash_menu/lua/autorun/cm_config.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/crash_menu/lua/autorun/cm_config.lua
@@ -7,10 +7,10 @@ CM.DelayTime = 30
 CM.Title = "Fuck Sake"
 
 //What message do you want to display when the server has crashed?
-CM.Message = "Looks like the server has crashed or been restarted, auto reconnecting in 90 seconds"
+CM.Message = "Looks like the server has crashed or been restarted, auto reconnecting in 60 seconds"
 
 //What is the estimated time in seconds it takes for the server to restart after a crash?
-CM.ServerRestartTime = 90
+CM.ServerRestartTime = 60
 
 CM.BackgroundColor = Color(52, 152, 219)
 
