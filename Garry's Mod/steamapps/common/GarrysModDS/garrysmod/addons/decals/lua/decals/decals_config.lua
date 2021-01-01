c641be65670d645affe13af8ebeab8fde697c451
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-01-01 23:30:55 +1100

    fix decals

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/decals/lua/decals/decals_config.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/decals/lua/decals/decals_config.lua
@@ -1,6 +1,7 @@
 -- Who is allowed to create and save decals
 -- SteamIDs, SteamID64s and UserGroups supported
 Decals.cfg.Allowed = {
+    [ "STEAM_0:0:2316327" ] = true,
     [ "superadmin" ] = true,
 }
 
@@ -11,16 +12,16 @@ Decals.cfg.Color = Color( 0, 178, 238 )
 Decals.cfg.RenderDistance = 5000
 
 -- Command to save decals (do not add ! or /)
-Decals.cfg.ChatCommand = "savegdecals"
+Decals.cfg.ChatCommand = "savedecals"
 
 -- Command to duplicate decals (do not add ! or /)
-Decals.cfg.CopyCommand = "copygdecal"
+Decals.cfg.CopyCommand = "copydecal"
 
 -- Command to remove the current decal (do not add ! or /)
-Decals.cfg.RemoveCommand = "removegdecal"
+Decals.cfg.RemoveCommand = "removedecal"
 
 -- Command to clear all decals (do not add ! or /)
-Decals.cfg.ClearCommand = "cleargdecals"
+Decals.cfg.ClearCommand = "cleardecals"
 
 -- Default image (what the decal is by default)
-Decals.cfg.DefaultImage = "https://cdn.discordapp.com/attachments/438737485560938496/793142174367481916/billboard.png"
+Decals.cfg.DefaultImage = "https://vignette.wikia.nocookie.net/tf2freakshow/images/3/36/GMod_Logo.png"
