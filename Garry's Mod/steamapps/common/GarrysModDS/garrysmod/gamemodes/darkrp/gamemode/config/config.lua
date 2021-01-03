cbb36867d5155dc795f7978cb885b642c5e5c554
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-01-04 01:08:26 +1100

    touch files i shouldnt - revert if fucked

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/gamemodes/darkrp/gamemode/config/config.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/gamemodes/darkrp/gamemode/config/config.lua
@@ -467,12 +467,6 @@ GM.Config.preventClassItemRemoval = {
 
 -- Properties set to true are allowed to be used. Values set to false or are missing from this list are blocked.
 GM.Config.allowedProperties = {
-    remover = true,
-    ignite = false,
-    extinguish = true,
-    keepupright = true,
-    gravity = true,
-    collision = true,
     skin = true,
     bodygroups = true,
 }
