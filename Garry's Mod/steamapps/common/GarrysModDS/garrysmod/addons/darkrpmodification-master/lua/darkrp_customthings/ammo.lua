8ce8b753cf46ba85738a97f68c477e1c61328663
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-01-04 23:51:41 +1100

    increase amount of ammo inside crates

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/ammo.lua
@@ -5,40 +5,40 @@ DarkRP.createAmmoType("pistol", {
 	name = "Pistol Ammo",
 	model = "models/Items/BoxSRounds.mdl",
 	price = 200,
-	amountGiven = 20
+	amountGiven = 60
 })
 
 DarkRP.createAmmoType("357", {
 	name = "Heavy Pistol Ammo",
 	model = "models/Items/357ammobox.mdl",
 	price = 300,
-	amountGiven = 20
+	amountGiven = 60
 })
 
 DarkRP.createAmmoType("buckshot", {
 	name = "Shotgun Ammo",
 	model = "models/Items/BoxBuckshot.mdl",
 	price = 400,
-	amountGiven = 8
+	amountGiven = 24
 })
 
 DarkRP.createAmmoType("smg1", {
 	name = "SMG Ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 500,
-	amountGiven = 30
+	amountGiven = 90
 })
 
 DarkRP.createAmmoType("ar2", {
 	name = "Rifle Ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 1500,
-	amountGiven =30
+	amountGiven = 120
 })
 
 DarkRP.createAmmoType("SniperRound", {
 	name = "Sniper Ammo",
 	model = "models/Items/BoxMRounds.mdl",
 	price = 2500,
-	amountGiven = 10
+	amountGiven = 30
 })
