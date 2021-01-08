7221684c9820dc3f01572fa1385fe55ba020d74b
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-01-08 16:25:03 +1100

    Revert "double oil spot capacity"
    This reverts commit 7e289ce51e50dc39d1c6949bc3d037c16ff7ab04.

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/zeros_oilrush_1.7.3/lua/zrush_config_oilspot.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/zeros_oilrush_1.7.3/lua/zrush_config_oilspot.lua
@@ -9,7 +9,7 @@ zrush.f.CreateOilSource({
     chance = 50,
     depth = 15,
     burnchance = 0,
-    oil_amount = math.Round(math.random(300, 2000)),
+    oil_amount = math.Round(math.random(300, 1000)),
     gas_amount = math.Round(math.random(100, 500)),
     chaos_chance = 5
 })
@@ -18,7 +18,7 @@ zrush.f.CreateOilSource({
     chance = 30,
     depth = 20,
     burnchance = 0,
-    oil_amount = math.Round(math.random(600, 4000)),
+    oil_amount = math.Round(math.random(600, 2000)),
     gas_amount = math.Round(math.random(500, 800)),
     chaos_chance = 10
 })
@@ -27,7 +27,7 @@ zrush.f.CreateOilSource({
     chance = 20,
     depth = 25,
     burnchance = 0,
-    oil_amount = math.Round(math.random(1500, 8000)),
+    oil_amount = math.Round(math.random(1500, 4000)),
     gas_amount = math.Round(math.random(500, 1000)),
     chaos_chance = 20
 })
