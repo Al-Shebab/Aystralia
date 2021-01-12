e53f797e5b6f9a690017251be87b044e457c7ad7
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-01-13 02:14:02 +1100

    Revert "fuck off 11 workshop things"
    This reverts commit 2f66ddf650de3f0251b51445259e290183031133.

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/aTags-Tags-Creator/lua/autorun/atags.lua
+++
@@ -1,4 +0,0 @@
--- ######### Made by Ikefi --- http://steamcommunity.com/id/ikefi/ ######### --
-
-AddCSLuaFile( "atags/sh_includes.lua" )
-include( "atags/sh_includes.lua" )
--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/aTags-Tags-Creator/lua/ev_plugins/atags_priv.lua
+++
@@ -1,7 +0,0 @@
-local PLUGIN = {}
-PLUGIN.Title = "aTags Admin Panel Access"
-PLUGIN.Description = "Access to the admin panel in aTags."
-PLUGIN.Author = "ikefi"
-PLUGIN.Privileges = { "atags adminpanel" }
-
-evolve:RegisterPlugin( PLUGIN )
