950ceb9db880dd870cd3f0d73b49a0463aa71e3e
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-01-15 12:29:53 +1100

    make have 2 printers worth it

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/oneprint/lua/oneprint/config.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/oneprint/lua/oneprint/config.lua
@@ -14,7 +14,7 @@ OnePrint.Cfg.ServerLimit = {                -- Max servers per printer (dependin
 ]]--
 
 -- Money
-OnePrint.Cfg.MoneyDelay = 15                 -- Cooldown before the printer generates money
+OnePrint.Cfg.MoneyDelay = 14                 -- Cooldown before the printer generates money
 OnePrint.Cfg.ServerIncome = 100              -- Income per server (for 1 server and without any upgrade)
 OnePrint.Cfg.OverclockingIncome = 25        -- % of income added per level of overclocking
 OnePrint.Cfg.ServerStorage = 500000           -- Max storage per server
