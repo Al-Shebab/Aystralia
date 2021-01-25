cd279837f83cabf4bedb94047b7f74ba67dea6a1
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-01-26 00:56:15 +1100

    add Jaspereeno's job

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
@@ -1107,6 +1107,30 @@ TEAM_SOWAKA = DarkRP.createJob("Sowaka", {
     CustomCheckFailMsg = "This job is for Gwacko only.",
 })
 
+TEAM_KERMIT = DarkRP.createJob("Kermit", {
+    color = Color(255, 0, 0, 255),
+    model = {"models/player/kermit.mdl"},
+    description = [[If you see this, remind Al Shebab.]],
+    weapons = {"pro_lockpick", "m9k_barret_m82", "invisibility_cloak"},
+    command = "TEAM_KERMIT",
+    max = 1,
+    salary = 8000,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "Donator Classes",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(100)
+        ply:SetHealth(100)
+        ply:SetArmor(100)
+    end,
+    customCheck = function(ply) return
+        table.HasValue({"STEAM_0:0:103364981", "STEAM_0:0:579690494"}, ply:SteamID())
+    end,
+    CustomCheckFailMsg = "This job is for Jaspereeno only.",
+})
+
 
 GAMEMODE.DefaultTeam = TEAM_CITIZEN
 
@@ -1123,4 +1147,5 @@ GAMEMODE.CivilProtection = {
 
 DarkRP.addHitmanTeam(TEAM_HITMAN)
 DarkRP.addHitmanTeam(TEAM_MERCENARY)
+DarkRP.addHitmanTeam(TEAM_KERMIT)
 DarkRP.addHitmanTeam(TEAM_ASSASSIN)
