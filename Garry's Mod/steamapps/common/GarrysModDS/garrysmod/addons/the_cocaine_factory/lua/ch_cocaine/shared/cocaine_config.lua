8f8e2427d7de795ab266c3801dd24b18b7618bc6
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-01 00:58:08 +1100

    fix coke prices

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/the_cocaine_factory/lua/ch_cocaine/shared/cocaine_config.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/the_cocaine_factory/lua/ch_cocaine/shared/cocaine_config.lua
@@ -31,8 +31,8 @@ TCF.Config.NPCDisplayDescription = "Sell me your snow." -- The description displ
 TCF.Config.SellDistance = 15000 -- How far away can the NPC detect your drug holder.
 TCF.Config.RandomPayoutInterval = 300 -- How many seconds before the randomized payout is changed again. It randomizes the settings below.
 
-TCF.Config.PayPerPackMin = 100000 -- How much should each cocaine pack be worth? Minimum value (without any donator bonuses)
-TCF.Config.PayPerPackMax = 145000 -- How much should each cocaine pack be worth? Maximum value (without any donator bonuses)
+TCF.Config.PayPerPackMin = 25000 -- How much should each cocaine pack be worth? Minimum value (without any donator bonuses)
+TCF.Config.PayPerPackMax = 27500 -- How much should each cocaine pack be worth? Maximum value (without any donator bonuses)
 
 TCF.Config.DisplayUIDistance = 300000 -- How far away from the drug dealer will the over-heads display show up? [Default = 300000]
 
