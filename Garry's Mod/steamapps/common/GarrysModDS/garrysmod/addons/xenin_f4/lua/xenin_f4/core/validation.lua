d1ec733db6bf93b830a5327238efb59f54bdc1fe
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-01 02:34:26 +1100

    add brand new f4 menu

---
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/xenin_f4/lua/xenin_f4/core/validation.lua
@@ -0,0 +1,20 @@
+--[[
+Generated using LAUX.
+Author: Patrick Ratzow (sleeppyy)
+
+Compile ID: 76561198174514595
+
+This is not the source code, this is a LAUX -> Lua transpiled version.
+
+Licensed to 76561198166995690
+Under no circumstances are you allowed to share this script.
+Doing so will get you banned from GmodStore.
+]]
+
+XeninUI.Scripts:register("xenin_f4menu", "F4 Menu", "2.3.0", "The Xenin Team", {
+  icon = "Tpm965d",
+  firstParty = true,
+  licensee = "76561198166995690",
+  licenseeProof = "76561198166995690",
+  versionCheck = true
+})
