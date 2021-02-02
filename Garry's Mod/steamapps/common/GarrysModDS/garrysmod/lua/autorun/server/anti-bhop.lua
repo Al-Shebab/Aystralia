7211114a8c1d6dabd86a79f5b4cf2a85cb4a0d68
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-02 13:45:53 +1100

    try to fix double jump

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/lua/autorun/server/anti-bhop.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/lua/autorun/server/anti-bhop.lua
@@ -1,5 +1,5 @@
 --Anti Bunny Hopping / Bhop.
 hook.Add("OnPlayerHitGround", "Anti-Bhop", function(player)
 	--When player touches ground take away and divide their velocity by two (in half).
-	player:SetVelocity(- player:GetVelocity() / 2)
+	player:SetVelocity(- player:GetVelocity() / 1.3)
 end)
