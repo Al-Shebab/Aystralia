993118dc6fadc4b0527c3c6ee83a12a20425fa85
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-08 14:21:46 +1100

    default job

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrpmodification-master/lua/darkrp_customthings/jobs.lua
@@ -14,7 +14,35 @@ Add your custom jobs under the following line:
 
 
 -- Civilians --
-
+TEAM_CITIZEN = DarkRP.createJob("Citizen", {
+    color = Color(74, 255, 69, 255),
+    model = {
+        "models/player/Group01/Female_01.mdl",
+        "models/player/Group01/Female_02.mdl",
+        "models/player/Group01/Female_03.mdl",
+        "models/player/Group01/Female_04.mdl",
+        "models/player/Group01/Female_06.mdl",
+        "models/player/group01/male_01.mdl",
+        "models/player/Group01/Male_02.mdl",
+        "models/player/Group01/male_03.mdl",
+        "models/player/Group01/Male_04.mdl",
+        "models/player/Group01/Male_05.mdl",
+        "models/player/Group01/Male_06.mdl",
+        "models/player/Group01/Male_07.mdl",
+        "models/player/Group01/Male_08.mdl",
+        "models/player/Group01/Male_09.mdl"
+    },
+    description = [[The Citizen is the most basic level of society you can hold besides being a hobo. You have no specific role in city life.]],
+    weapons = {},
+    command = "citizen",
+    max = 0,
+    salary = 500,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Citizens",
+})
 JOB_CASINO_SECURITY = DarkRP.createJob("Casino Security", {
     color = Color(74, 255, 69, 255),
     model = {"models/player/portal/Male_04_security.mdl"},
