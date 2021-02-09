346bb13907e0c389bcbdf5e3d6ef753c858b987f
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-09 13:23:21 +1100

    buff silver and gold

--- Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/zeros_retrominingsystem_2.2.8/lua/zrmine_config.lua
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/zeros_retrominingsystem_2.2.8/lua/zrmine_config.lua
@@ -244,8 +244,8 @@ local Bronze_1kg = 700 //$
 local Iron_1kg = 550 //$
 */
 
-local Gold_1kg = 2700 //$
-local Silver_1kg = 1200 //$
+local Gold_1kg = 2800 //$
+local Silver_1kg = 1400 //$
 local Bronze_1kg = 700 //$
 local Iron_1kg = 550 //$
 
