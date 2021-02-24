b75437936b57fbb787fca1375399df246cc5f8b3
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-02-25 02:24:53 +1100

    add new previous ban message

---
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/Ban-Message-Gmod-Server/lua/pbanmsg_config.lua
@@ -0,0 +1,23 @@
+PBanMsg = {}
+PBanMsg.ban_message = [[
+======================
+  BANNED FROM SERVER
+======================
+Time Remaining:
+  <time>
+Admin Name: 
+  <admin>
+Reason:
+  <reason>
+]]
+
+PBanMsg.badpw_message = [[
+======================
+  BAD PASSWORD GIVEN
+    ADMINS NOTIFIED
+======================
+]]
+
+PBanMsg.notifyAdmins_BadPW = true;
+
+PBanMsg.notifyAdmins_BannedUser = true;
