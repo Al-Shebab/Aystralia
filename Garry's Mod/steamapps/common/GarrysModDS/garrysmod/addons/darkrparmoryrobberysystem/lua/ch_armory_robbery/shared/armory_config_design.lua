678e356305e338aa496bc43d13d7492576f33aa3
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-04-13 21:19:16 -0700

    fix fuck up

---
+++ Garry's Mod/steamapps/common/GarrysModDS/garrysmod/addons/darkrparmoryrobberysystem/lua/ch_armory_robbery/shared/armory_config_design.lua
@@ -0,0 +1,31 @@
+-- Design options for the armory entity display.
+CH_ArmoryRobbery.Design.ArmoryTextColor = Color( 255, 255, 255, 255 )
+CH_ArmoryRobbery.Design.ArmoryTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.CooldownTextColor = Color( 230, 230, 230, 255 )
+CH_ArmoryRobbery.Design.CooldownTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.CountdownTextColor = Color( 230, 230, 230, 255 )
+CH_ArmoryRobbery.Design.CountdownTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.CooldownTimerTextColor = Color( 100, 100, 100, 255 )
+CH_ArmoryRobbery.Design.CooldownTimerTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.CountdownTimerTextColor = Color( 100, 100, 100, 255 )
+CH_ArmoryRobbery.Design.CountdownTimerTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.ScreenColor = Color( 60, 60, 60, 200 )
+CH_ArmoryRobbery.Design.ScreenBoxColor = Color( 1, 1, 1, 200 )
+
+CH_ArmoryRobbery.Design.MoneyTextColor = Color( 60, 100, 60, 255 )
+CH_ArmoryRobbery.Design.MoneyTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.AmmoTextColor = Color( 60, 60, 100, 255 )
+CH_ArmoryRobbery.Design.AmmoTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.ShipmentsTextColor = Color( 100, 60, 60, 255 )
+CH_ArmoryRobbery.Design.ShipmentsTextBoarder = Color( 0, 0, 0, 255 )
+
+CH_ArmoryRobbery.Design.TheYes = Color( 0, 153, 0, 255 )
+CH_ArmoryRobbery.Design.TheNo = Color( 153, 0, 0, 255 )
+CH_ArmoryRobbery.Design.TheBoarder = Color( 0, 0, 0, 255 )
